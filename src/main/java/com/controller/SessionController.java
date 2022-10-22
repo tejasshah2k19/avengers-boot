86a50b6e9422c8b8d4c3bc91ba32e4c48c7b4ae1
tejas <tejasshah@gmail.com>
2022-10-22 19:00:51 +0530

    db insert

--- src/main/java/com/controller/SessionController.java
+++ src/main/java/com/controller/SessionController.java
@@ -1,14 +1,30 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.bean.UserBean;
+import com.dao.UserDao;
 
 @Controller
 public class SessionController {
+
+	@Autowired
+	UserDao userDao;
 
 	@GetMapping("/signup")
 	public String signup() {
 		System.out.println("Signup()");
 		return "Signup";
+	}
+
+	@PostMapping("/saveuser")
+	public String saveUser(UserBean user) {
+		System.out.println(user.getFirstName());
+		System.out.println(user.getEmail());
+		userDao.insertUser(user);
+		return "Home";
 	}
 }
