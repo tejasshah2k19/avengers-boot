e767d9ff514f0c9f7347fd5b886e3f94c5cafcd9
tejas <tejasshah@gmail.com>
2022-11-14 21:02:15 +0530

    delete view update

--- src/main/java/com/controller/ProductController.java
+++ src/main/java/com/controller/ProductController.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,10 +34,39 @@ public class ProductController {
 		return product;// object -> json
 	}
 
-	@GetMapping("/product")
+	@GetMapping("/products")
 	public List<ProductBean> getAllProducts() {
 		List<ProductBean> products = new ArrayList<ProductBean>();
 		return products;
 	}
+
+	// /product/12
+	@DeleteMapping("/product/{productId}")
+	public List<ProductBean> deleteProduct(@PathVariable("productId") int productId) {
+		ProductBean deletedBean = productDao.getProductById(productId);
+
+		productDao.deleteProduct(productId);
+		List<ProductBean> products = new ArrayList<ProductBean>();
+		return products;
+	}
+
+	@GetMapping("/product/{productId}")
+	public ProductBean getProductByProductId(@PathVariable("productId") int productId) {
+		ProductBean productBean = productDao.getProductById(productId);
+		return productBean;
+	}
+	
+	@PutMapping("/product")
+	public ProductBean updateProduct(@RequestBody ProductBean product) {
+		System.out.println(product.getName());
+		System.out.println(product.getPrice());
+		System.out.println(product.getQty());
+
+		productDao.updateProduct(product);
+
+		System.out.println("Product updated....");
+		return product;// object -> json
+	}
+	
 
 }
--- src/main/java/com/dao/ProductDao.java
+++ src/main/java/com/dao/ProductDao.java
@@ -16,4 +16,18 @@ public class ProductDao {
 		stmt.update("insert into products (name,price,qty) values (?,?,?)", product.getName(), product.getPrice(),
 				product.getQty());
 	}
+
+	public void deleteProduct(int productId) {
+		stmt.update("delete from products where productid = ? ", productId);
+
+	}
+
+	public ProductBean getProductById(int productId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void updateProduct(ProductBean product) {
+		// update query
+	}
 }
